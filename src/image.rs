5d26ed1bb93e605c5165a20382459424ec5921b6
sandrololo <sandro2000h@gmail.com>
2025-10-23 16:30:39 +0000

    store height and width along image vector

--- src/image.rs
+++ src/image.rs
@@ -1,22 +1,38 @@
 use std::fs::File;
-use tiff::decoder::Decoder;
+use tiff::decoder::{Decoder, DecodingResult};
+
+pub struct Image<T> {
+    pub height: u32,
+    pub width: u32,
+    pub data: Vec<T>,
+}
 
 pub struct SurfaceAmplitudeImage {
-    pub surface: Vec<f32>,
-    pub amplitude: Vec<f32>,
+    pub surface: Image<f32>,
+    pub amplitude: Image<f32>,
 }
 
 impl SurfaceAmplitudeImage {
     pub fn from_file(path: &str) -> anyhow::Result<Self> {
         let img_file = File::open(path)?;
         let mut decoder = Decoder::new(img_file)?;
+        let dimensions = decoder.dimensions()?;
         let surface = match decoder.read_image()? {
-            tiff::decoder::DecodingResult::F32(data) => Ok(data),
+            DecodingResult::F32(data) => Ok(Image {
+                height: dimensions.1,
+                width: dimensions.0,
+                data,
+            }),
             _ => Err(anyhow::anyhow!("Unsupported image format")),
         }?;
         decoder.next_image()?;
+        let dimensions = decoder.dimensions()?;
         let amplitude = match decoder.read_image()? {
-            tiff::decoder::DecodingResult::F32(data) => Ok(data),
+            DecodingResult::F32(data) => Ok(Image {
+                height: dimensions.1,
+                width: dimensions.0,
+                data,
+            }),
             _ => Err(anyhow::anyhow!("Unsupported image format")),
         }?;
         Ok(Self { surface, amplitude })
